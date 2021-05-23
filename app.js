fbf6286b691e2925f60cbdd5636d1ca30a3ebb40
Ijlal Ahmad <ijlalahmad845@gmai.com>
2021-05-23 08:34:25 +0530

    Added jumbling mode acc to user input

--- app.js
+++ app.js
@@ -43,8 +43,14 @@ venom
 
 function start(client) {
     client.onMessage((message) => {
-    if (message.body == 'Bingo' || message.body == 'bingo') {
-        output = result;
+        if (message.body == 'Bingo' || message.body == 'bingo') {
+            output = "Welcome to Bingo!!!\n" +
+                "Type \"Bingo number\" to cut that number\n" +
+                "But the board will jumble acc to your input\n" +
+                "prime number - no jumble\n" +
+                "odd numbers - maybe or may not be jumble\n" +
+                "even numbers - Always jumble";
+        output += result;
       client
         .reply(message.from, output, message.id.toString())
         .then((result) => {
@@ -67,7 +73,17 @@ function start(client) {
             else if (numbers[i].length == 2) {
                 if (parseInt(numbers[i]) == parseInt(cutnumber)) {
                     numbers[i] = '~' + numbers[i] + '~';
-                    jumble();
+
+                    if (cutnumber == 2 || cutnumber == 3 || cutnumber == 5 || cutnumber == 7 || cutnumber == 11 || cutnumber == 13 || cutnumber == 17 || cutnumber == 23)
+                        outputinit();
+                    else if (cutnumber % 2 == 0)
+                        jumble();
+                    else if (cutnumber % 2 == 1){
+                        if (Math.round(Math.random() * 10) % 2 == 0)
+                            jumble();
+                        else outputinit();
+                    }
+                
                     output = result;
                     break;
                 }
